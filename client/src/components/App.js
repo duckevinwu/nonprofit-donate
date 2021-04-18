4898912cd024ce45c30fe1fa31247aad5564ccf1
Kevin <kevinwu97@gmail.com>
2021-04-18 00:23:12 -0500

    fix typo

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -30,7 +30,7 @@ export default class App extends React.Component {
 							exact
 							path="/temp"
 							render={() => (
-								LandingPage
+								<LandingPage />
 							)}
 						/>
 						<Route
